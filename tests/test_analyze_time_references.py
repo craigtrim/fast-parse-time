ee75d077fdc866e25b34d37695a6e053b9c48289
Craig Trim <craigtrim@gmail.com>
2022-08-16 11:40:23 -0700

    Fix Test Case

--- tests/test_analyze_time_references.py
+++ tests/test_analyze_time_references.py
@@ -6,32 +6,33 @@ assert svc
 
 def test_01():
     input_text = 'from here show me all 5 items all the history from 5 days ago'
-    solutions = svc.process(input_text)
-    assert len(solutions) == 1
-    assert solutions == [{'Cardinality': 5, 'Frame': 'day', 'Tense': 'past'}]
+    d_result = svc.process(input_text)
+    assert len(d_result['result']) == 1
+    assert d_result['result'] == [
+        {'Cardinality': 5, 'Frame': 'day', 'Tense': 'past'}]
 
 
 def test_02():
     input_text = 'the explosion happened 5 minutes ago'
-    solutions = svc.process(input_text)
-    assert len(solutions) == 1
-    assert solutions == [
+    d_result = svc.process(input_text)
+    assert len(d_result['result']) == 1
+    assert d_result['result'] == [
         {'Cardinality': 5, 'Frame': 'minute', 'Tense': 'past'}]
 
 
 def test_03():
     input_text = 'I want to see my history for the past 30 minutes'
-    solutions = svc.process(input_text)
-    assert len(solutions) == 1
-    assert solutions == [
+    d_result = svc.process(input_text)
+    assert len(d_result['result']) == 1
+    assert d_result['result'] == [
         {'Cardinality': 30, 'Frame': 'minute', 'Tense': 'past'}]
 
 
 def test_04():
     input_text = 'show me all records from the last couple of weeks please'
-    solutions = svc.process(input_text)
-    assert len(solutions) == 1
-    assert solutions == [
+    d_result = svc.process(input_text)
+    assert len(d_result['result']) == 1
+    assert d_result['result'] == [
         {'Cardinality': 2, 'Frame': 'week', 'Tense': 'past'}]
 
 
