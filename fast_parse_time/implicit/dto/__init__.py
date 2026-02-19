4e7a239e4fe5339c66da821a41439507a18b810c
Craig Trim <craigtrim@gmail.com>
2026-02-18 18:10:15 -0600

    chore: Remove dead index_by_frame_kb and ignore .bak files

--- fast_parse_time/implicit/dto/__init__.py
+++ fast_parse_time/implicit/dto/__init__.py
@@ -2,7 +2,6 @@
 # -*- coding: UTF-8 -*-
 
 
-from .index_by_frame_kb import d_index_by_frame_kb
 from .index_by_keyterm_kb import d_index_by_keyterm_kb
 from .index_by_slot_kb import d_index_by_slot_kb
 from .keyterm_counter_kb import d_keyterm_counter_kb
